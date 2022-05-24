b5ec1c75b1199d89c0d0d0b3ebe4fb5c945a9e81
Michael Taggart <michael.t.taggart@gmail.com>
2022-05-23 18:34:10 -0700

    Remove rm

--- 5-2_juice-shop/run.sh
+++ 5-2_juice-shop/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker container run -d --restart=on-failure --name juice_shop --rm -p 8002:3000 bkimminich/juice-shop
+docker container run -d --restart=on-failure --name juice_shop -p 8002:3000 bkimminich/juice-shop
