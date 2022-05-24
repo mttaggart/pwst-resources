66069ad18220ec0b8d7db25484554d3f97c21f39
Michael Taggart <michael.t.taggart@gmail.com>
2022-05-23 22:48:53 -0700

    unsafe for some attacks

--- 5-2_juice-shop/run.sh
+++ 5-2_juice-shop/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker container run -d --restart=on-failure --name juice_shop -p 8002:3000 bkimminich/juice-shop
+docker container run -d -e NODE_ENV=unsafe --restart=on-failure --name juice_shop -p 8002:3000 bkimminich/juice-shop
